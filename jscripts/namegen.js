adc78efa4eec75fe865e06c335a118209aa2a5c7
Heather Uebel <tessouhaits@gmail.com>
2018-07-18 21:03:18 -0400

    Merge branch 'master' of https://github.com/TheHeatherlyn/Code-Louisville-Capstone
    # Conflicts:
    #	Images/Thumbs.db
    #	jscripts/namegen.js

--- jscripts/namegen.js
+++ jscripts/namegen.js
@@ -9,16 +9,22 @@ var last = [
 
 ]
 
+var pics = [
+    
+    "images/80s-pink.jpg", "images/Armored.jpg", "images/Bent.jpg", "images/Big-belly.jpg", "images/Big-mama.jpg", "images/Blue.jpg", "images/Conjoined.jpg", "images/Crazy-cook.jpg", "images/cute.jpg", "images/Droopy-horns.jpg", "images/emo-troll.jpg", "images/Fuzzy-trap.jpg", "images/granny.jpg", "images/Grass-ass.jpg", "images/Grr.jpg", "images/miniature.jpg", "images/ole-one-eye.jpg", "images/pumpkin.jpg", "images/She-mini.jpg", "images/Surprised-troll.jpg", "images/Troll-play.jpg", "images/ugly.jpg", "images/Viking.jpg", "images/warty.jpg", "images/Wicked.jpg", "images/wiz.jpg", "images/Yucky.jpg"
+
+]
+
+
 function firstName() {
     function displayName() {
         var randomNumber = Math.floor(Math.random() * (first.length));
         var funnyName = first[randomNumber];
         var randomNumber = Math.floor(Math.random() * (last.length));
-        // Concatenating here allowed me to pull names from both arrays and string them together instead of running a loop. //
+        // Concatenating here allows me to pull names from both arrays and string them together instead of running a loop. //
         funnyName = funnyName.concat( ' ', last[randomNumber]);
         document.getElementById("first").innerHTML = funnyName;
-      
-        
+         
         
 
         
@@ -29,5 +35,11 @@ function firstName() {
     
 }
 
+
+function trollFace() {
+    var ranPic = Math.floor((Math.random() * (pics.length))); 
+
+    $("#funnyPic").attr("src", pics[ranPic]);
+}
 
 
